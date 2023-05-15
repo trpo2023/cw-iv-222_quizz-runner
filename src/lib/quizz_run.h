6516e019279e7ceb73d94a7ccfe3c5f78a1f3991
Pavel Gorodilov <NKtym@users.noreply.github.com>
2023-05-15 20:21:10 +0700

    Add quizz_run function, #3

--- src/lib/quizz_run.h
+++ src/lib/quizz_run.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "quizz_create.h"
+#include <lib/quizz_create.h>
 
 struct user{
 	char username[100];
@@ -18,6 +18,7 @@ struct answers{
 int get_username(struct user* users);
 int quizz_select(struct user* users);
 int get_pass_percentage(struct answers* answer,struct user* users,int questionCount);
-int print_answer_options(struct quizz* quiz,int questionCount);
-struct answers get_answer(struct answers* results,int optionCount);
+void print_answer_options(struct quizz* quiz,int questionCount);
+int get_answer(struct answers* results,int optionCount);
 int check_answers(struct quizz* quiz,struct answers* results,int questionCount,int optionCount);
+struct user* run_quizz(struct answers* answer,struct user* users, struct quizz* quiz);
