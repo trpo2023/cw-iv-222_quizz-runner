18b35f7006805e94a70519cb437216a212c83d20
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-05-22 18:44:56 +0700

    Fix codestyle, #4

--- src/lib/quizz_run.h
+++ src/lib/quizz_run.h
@@ -13,16 +13,15 @@ struct answers {
     int isCorrect[100];
 };
 
-int get_username(struct user* users,char* argument);
-int quizz_select(struct user* users,int* argument);
+int get_username(struct user* users, char* argument);
+int quizz_select(struct user* users, int* argument);
 int get_pass_percentage(
         struct answers* answer, struct user* users, int questionCount);
 void print_answer_options(struct quizz* quiz, int questionCount);
-int get_answer(struct answers* results, int optionCount,char* ch);
+int get_answer(struct answers* results, int optionCount, char* ch);
 int check_answers(
         struct quizz* quiz,
         struct answers* results,
         int questionCount,
         int optionCount);
-struct user*
-run_quizz();
+struct user* run_quizz();
